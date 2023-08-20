8e25ccae87abcb9a6b58c3686d28c1002aaa1e78
ricardo-silvasantos <ricosilvasantos@yahoo.com.br>
2023-08-20 15:52:49 -0300

    resolução do exercicio

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string nome, sobrenome;
+        Console.Write("Digite seu nome..... ");
+        nome = Console.ReadLine();
+        Console.Write("Digite seu sobrenome..... ");
+        sobrenome = Console.ReadLine();
+        Console.WriteLine($"{nome}{sobrenome}");
+        Console.WriteLine($"{sobrenome.ToUpper()}, {nome}");
+    }
+}
